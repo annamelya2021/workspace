a6542523fce66c899465e74bb07694cdaf6c826d
Anna Melnychenko <88847909+annamelya2021@users.noreply.github.com>
2024-10-09 12:13:48 +0200

    3

---
+++ my-dashboard/src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heavy-loaders-slow'
+  standalone: true,
+  imports: [],
+  template: `<h1>Hola slow</h1>`,
+  styles: ``
+})
+export class HeavyLoadersSlowComponent {
+
+}
